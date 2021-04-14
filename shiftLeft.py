d9099bf4bc2870e4537b2781ba7c2ef653c496c7
Matt Brundage II <mattjbrundage@icloud.com>
2021-04-14 10:20:23 -0400

    Change

--- shiftLeft.py
+++ shiftLeft.py
@@ -15,12 +15,3 @@ result = shiftLeft + string[0]
 
 #Print result
 print(result)
-
-"""
-Dr. Brown, I have tried this damn thing so many different ways
-I know that I am getting an index error because my input is an integer
-Everytime I try to convert to a string I get a whole slew of errors
-You'll see I wrote the two scripts completely different ways
-I've googled this to death
-I will be excited to see your comments on how to fix this
-"""
